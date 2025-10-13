e13bce492f28db33fd989a526983112733ac270f
Avinep123 <avipachhai01@gmail.com>
2025-10-13 21:23:52 +0545

    components

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Card from './components/Card.jsx'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 const App = () => {
   return (
     <div>
+      <Navbar />
       <div className='hello'>
         <Card /></div>
     </div>
---
+++ first-project/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='nav'>
+            hello this is Nav
+        </div>
+    )
+}
+
+export default Navbar
