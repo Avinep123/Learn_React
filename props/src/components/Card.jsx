626e73fa6e440301f448b9e04f1b2ffb297733c7
Avinep123 <avipachhai01@gmail.com>
2025-10-14 15:53:13 +0545

    hello

---
+++ props/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const card = () => {
+    return (
+        <div>
+            <div className='card'>
+                <img src='https://images.unsplash.com/photo-1733758839991-a750b4ae0e7a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764' alt='hello'></img>
+                <h1>Avinay</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <button>View Profile</button>
+            </div>
+        </div>
+    )
+}
+
+export default card
