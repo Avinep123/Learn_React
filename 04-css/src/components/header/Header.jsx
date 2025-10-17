cf61c907ad045254a388a2a09f0687aae2ac81a6
Avinep123 <avipachhai01@gmail.com>
2025-10-17 20:36:35 +0545

    css

---
+++ 04-css/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div className={style.header} ><h3>Avinay</h3>
+            <button className='btn'>hello</button></div>
+    )
+}
+
+export default Header
