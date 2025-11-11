7f855a092013f3c6c0a0f075f105930041ee0456
Avinep123 <avipachhai01@gmail.com>
2025-11-11 18:22:08 +0545

    notes-project

--- 13-notes-project/src/App.jsx
+++ 13-notes-project/src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
 
   return (
     <>
-      <div className='h-full bg-black text-white lg:flex'>
+      <div className='h-screen bg-black text-white lg:flex'>
 
 
         <form
@@ -60,9 +60,9 @@ const App = () => {
 
         </form>
 
-        <div className='lg:w-1/2  lg:border-l-2 p-10'>
+        <div className='flex flex-col  lg:w-1/2  lg:border-l-2 p-10 bg-black'>
           <h1 className='text-4xl font-bold'>Your notes</h1>
-          <div className='flex flex-wrap items-start justify-start gap-5 mt-5 h-full overflow-auto'>
+          <div className='flex flex-wrap items-start justify-start gap-5 mt-5  h-screen overflow-auto'>
             {
               task.map(function (elem, idx) {
                 return <div key={idx} className=' flex flex-col justify-between items-start h-52 w-40 pt-8 pb-4 relative px-4 text-black rounded-xl bg-[url(https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png)] bg-cover '>
@@ -70,7 +70,7 @@ const App = () => {
                     <h3 className='leading-tight text-xl font-bold'>
                       {elem.title}
                     </h3>
-                    <p className='mt-4 leading-tight font-medium text-gray-500'>
+                    <p className='mt-4 leading-tight font-semi text-xs text-gray-500'>
                       {elem.detail}
                     </p>
                   </div>
