8be3082ce78c3af2e8cff2597f52b918f7ec50a8
Avinep123 <avipachhai01@gmail.com>
2025-11-12 21:48:39 +0545

    apicalls

---
+++ 15-apicalls/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+
+    function getdata() {
+
+    }
+    return (
+        <div>
+            <button onClick={getdata}>Get Data</button>
+        </div>
+    )
+}
+
+export default App
