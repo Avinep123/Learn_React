2f0f197d17b225a92b5cb1b194559ff236191919
Avinep123 <avipachhai01@gmail.com>
2025-11-14 19:48:16 +0545

    apicalls

--- 15-apicalls/src/App.jsx
+++ 15-apicalls/src/App.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
+import axios from 'axios'
 
 const App = () => {
 
-    function getdata() {
+    /*Using fetch method*/
+    /*Using Normal Function*/
+    async function getdat() {
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+        console.log(response)
+
+
+
 
     }
+
+    /*Using Arrow Function*/
+    const getData = async () => {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+
+        const data = await resp.json()
+        console.log(data)
+    }
+
+    /*Using Axios*/
+
+
+    const getDataAxios = async () => {
+
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        console.log(response.data)
+
+    }
+
+
+
     return (
         <div>
-            <button onClick={getdata}>Get Data</button>
+            <button onClick={getData}>Get Data</button>
+            <button onClick={getDataAxios}>Get Data</button>
         </div>
     )
 }
